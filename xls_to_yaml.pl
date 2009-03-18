418d538873cc07c35610575ef5360afc93ff78a0
Lukhnos D. Liu <lukhnos@lithoglyph.com>
2009-03-18 14:53:04 +0800

    Uses perl to convert xls.

---
+++ xls_to_yaml.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl -CS
+
+use strict;
+use utf8;
+use Spreadsheet::ParseExcel;
+use Spreadsheet::ParseExcel::FmtJapan;
+use Encode;
+use YAML;
+
+my $sheet = 0;
+my $header = 0;
+
+my $workbook = Spreadsheet::ParseExcel::Workbook->Parse($ARGV[0], Spreadsheet::ParseExcel::FmtJapan->new);
+my $worksheet = $workbook->{Worksheet}[int $sheet];
+my $begin = $header ? 1 : 0;
+$|=1;
+
+my @output = ();
+
+for my $r ($begin..$worksheet->{MaxRow}) {
+    my @data=();
+    for my $c (0..$worksheet->{MaxCol}) {
+        my $cell=$worksheet->{Cells}[$r][$c];
+        my $v=(defined $cell) ? $cell->Value : "";
+        if ($cell->{Code} eq "ucs2") {
+            $v=Encode::decode("UCS2-BE", $v);
+        }
+        else {
+            $v=Encode::decode("iso8859-1", $v);
+        }
+
+        push @data, $v;
+    }
+    
+    push @output, \@data;
+    # print join(", ", @data), "\n";
+}
+
+
+print YAML::Dump(\@output);
--- yaml_to_ab.rb
+++ yaml_to_ab.rb
@@ -27,7 +27,7 @@ sab = ABAddressBook.sharedAddressBook
 
 rows = YAML.load(STDIN.readlines.join("\n"))
 colmap = rows.shift.inject([{}, 0]) { |m, k| [(m[0][k.downcase.gsub(/\W/, "").intern] = m[1] ; m[0]), m[1] + 1]  }[0]
-STDERR.puts colmap.to_yaml
+# STDERR.puts rows.to_yaml
 
 rows.each do | row |
   entry = colmap.keys.inject({}) { |m, k| m[k] = row[colmap[k]] || "" ; m }
@@ -59,7 +59,31 @@ rows.each do | row |
   phones = entry[:phones].split(/;/).map { |a| a.gsub(/^\s*(.+?)\s*$/, '\1') }
   person["Phone"] = add_multi_value(phones)    
   
-  person["Note"] = entry[:notes] + " abtool"
+  if entry[:addresses].length > 0  
+    addrs = entry[:addresses].componentsSeparatedByString(";").map { |a| a.gsub(/^\s*(.+?)\s*$/, '\1').gsub(/\s?\/\/\s?/, "\n") } 
+    
+    # puts addrs.join(">>>")
+    
+    if first_addr = addrs.shift
+        
+      mv = ABMutableMultiValue.new  
+      mv.addValue({ "Street" => first_addr }, :withLabel => "work")
+
+      while next_addr = addrs.shift
+        mv.addValue({ "Street" => next_addr }, :withLabel => "work")
+      end
+      mv.setPrimaryIdentifier(mv.identifierAtIndex(0))    
+      person["Address"] = mv
+    end
+  end
+
+  if entry[:notes].length > 0
+    n = entry[:notes] + "\nabtool" 
+    person["Note"] = n    
+  else
+    person["Note"] = "abtool"
+  end
+  
   sab.addRecord person
 end
 
